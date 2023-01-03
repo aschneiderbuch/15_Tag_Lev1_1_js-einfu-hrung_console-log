31bbb7607bcac53591acd8266c7324f3872b2853
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 13:09:47 +0100

    Ende

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+let a = 12
+let b = a * 3
+
+console.log(b)
+
+document.write(b)
+
+document.write("<h2>(b)</h2>")
+
+document.write(`<h2>{b}</h2>`)
+
+document.write("<h2>${b}</h2>")
+
+document.write(`<h2>${b}</h2>`)
+
+console.log(document.URL)
+
+console.log("Hallo World")
+
+console.log("Anton")
+
+let firstName = "Anton"
+console.log(firstName)
+
+let age = 28
+console.log(age)
+
+let job= "Trainer"
+console.log(job)
+
+let married = true                  // Boolen    ist in der Konsole auch farbe lila
+console.log(married)
+
+console.log(firstName + " ist " + age + " Jahre alt, von Beruf " + job + " und ist verheiratet: " + married) 
+
+document.write(`<h2>${firstName} ist ${age} Jahre alt, von Beruf ${job} und ist verheiratet: ${married}</h2>`)
